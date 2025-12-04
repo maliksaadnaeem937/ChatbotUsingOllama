0d5bd4ad31dc38cabb6c9f2f3c767b3c1fe72898
Malik Saad Naeem <156428364+maliksaadnaeem937@users.noreply.github.com>
2025-12-04 11:34:48 +0500

    secured

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,10 @@
 # database.py
 from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+MONGO_URL = os.getenv("MONGO_URL")
 
-MONGO_URL = "mongodb+srv://maliksaadnaeem937:rflMeVENy8K3FEQb@cluster0.0fuyh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"   # or your Atlas URL
+
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["chatbotdb"]  
+db = client[os.getenv("DbName")]  

4facfec15263f012c16cc0eff534a9a185bf940d
Malik Saad Naeem <156428364+maliksaadnaeem937@users.noreply.github.com>
2025-12-04 11:33:40 +0500

    secured

--- FrontEnd/src/utils/firebase-config.js
+++ FrontEnd/src/utils/firebase-config.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBsAwPsByaFxapNSH6xKBA3T5YrFc2EZwc",
-  authDomain: "nextjsauthaisemproject.firebaseapp.com",
-  projectId: "nextjsauthaisemproject",
-  storageBucket: "nextjsauthaisemproject.firebasestorage.app",
-  messagingSenderId: "124157741788",
-  appId: "1:124157741788:web:6369c22261fcd4453e4219",
-  measurementId: "G-6TRFZSLB9F"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const googleProvider = new GoogleAuthProvider();
